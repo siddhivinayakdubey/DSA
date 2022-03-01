ae7c00cd518456ff7d961d14d5808d68124e8110
Siddhivinayak Dubey <siddhivinayakdubey@gmail.com>
2022-03-01 21:54:28 +0530

    Add files via upload

--- Trees/Alloperations.py
+++ Trees/Alloperations.py
@@ -87,6 +87,33 @@ class Tree:
             if p.rchild is not None:
                 queue.append(p.rchild)
 
+    def count(self):
+        if self.root is None:
+            return 0
+        else:
+            print("(nodes,height)")
+            nodes=self.countnodes(self.root)
+            height=self.heightcount(self.root)
+            return nodes,height
+    def heightcount(self,p):
+        if p is not None:
+            x=self.heightcount(p.lchild)
+            y=self.heightcount(p.rchild)
+            if (x>y):
+                return x+1
+            else:
+                return y+1
+        return 0
+
+    def countnodes(self,p):
+            if p is not None:
+                x= self.countnodes(p.lchild)
+                y=self.countnodes(p.rchild)
+
+                return int(x+y+1)
+            else:
+                return 0
+
 
 
 
