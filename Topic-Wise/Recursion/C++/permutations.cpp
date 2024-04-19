21f504e9db6e28d780cfcce72cddf36fa6c7e641
siddhivinayakdubey <siddhivinayakdubey@gmail.com>
2024-04-19 15:45:28 +0530

    leftovers

--- Topic-Wise/Recursion/C++/permutations.cpp
+++ Topic-Wise/Recursion/C++/permutations.cpp
@@ -4,15 +4,18 @@
 #include "iostream"
 using namespace std;
 
-void permutes(string s,string combinations){
+
+void permutes(string s, string combinations){
+
     if(s.length()==0){
         cout<<combinations<<endl;
         return;
     }
+
     for(int i=0 ; i<s.length(); i++){
         char currchar = s[i];
-        string remaining = s.substr(0,i)+s.substr(i+1);
-        permutes(remaining,combinations+currchar);
+        string remaining = s.substr(0,i)+ s.substr(i+1);
+        permutes(remaining, combinations+currchar);
     }
 }
 
